a5b22db27f7d03ee97293505f48cf6a53aa65cb3
Brunoo-Lima <Bruno2.jean.lima@outlook.com>
2025-09-03 22:01:17 -0300

    feat: test initial

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/Button/Button';
+import { Text, View } from 'react-native';
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Teste</Text>
+
+      <Button text="Continuar" />
+    </View>
+  );
+}
---
+++ mobile/src/components/ui/Button/Button.tsx
@@ -0,0 +1,9 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+type IButtonProps = TouchableOpacityProps & {
+  text: string;
+};
+
+export const Button = ({ text }: IButtonProps) => {
+  return <TouchableOpacity>{text}</TouchableOpacity>;
+};
