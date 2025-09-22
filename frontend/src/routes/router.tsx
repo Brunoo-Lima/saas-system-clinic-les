6caeddfe02966d6243156c3f2f5406f255ed0e5e
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2025-09-22 16:30:27 -0300

    fix: fixed link to register screen

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -44,7 +44,7 @@ export const router = createBrowserRouter([
         element: <InsurancePage />,
       },
       {
-        path: '/agendamentos/historico',
+        path: '/agendamentos/historico-de-agendamentos',
         element: <HistoricAppointmentPage />,
       },
       {
