c38e3619a7e37587108b26a7a2b889a8aa52991b
Danilo de Araújo Leite Gomes <112290478+JayXani@users.noreply.github.com>
2025-10-30 23:04:15 -0300

    feat: Added the portuguese messages

--- backend/src/domain/validators/SchedulingDoctorValidator/DatesToBlockedValidator.ts
+++ backend/src/domain/validators/SchedulingDoctorValidator/DatesToBlockedValidator.ts
@@ -6,9 +6,9 @@ export class DatesToBlockedValidator implements IProcessValidator{
     async valid(datesToBlocked: Array<SchedulingBlockedDays>){
         try {
             const timesFiltered = datesToBlocked.filter((dtb) => dtb.dateBlocked).map((dtb) => dtb.dateBlocked?.getTime())
-            if(!timesFiltered.length) return ResponseHandler.error("Anyone dates to blocked is valid !")
+            if(!timesFiltered.length) return ResponseHandler.error("Nao existe datas validas para bloquear")
             const datesNotDuplicates = new Set(timesFiltered) 
-            if(datesNotDuplicates.size !== timesFiltered.length) return ResponseHandler.error("Cannot be inserted duplicate dates !")
+            if(datesNotDuplicates.size !== timesFiltered.length) return ResponseHandler.error("Não pode inserir datas duplicadas")
             
             return ResponseHandler.success(datesToBlocked, "The dates is valid, you can inserted !")
         } catch(e) {
